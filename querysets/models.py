2cb97aa9112be999ec4033227b51e20c38a56eec
Sankalp Boolchandani <boolchandanisankalp@gmail.com>
2025-12-10 01:07:04 +0530

    genrating data

--- querysets/models.py
+++ querysets/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=200)
+    cuisine_type = models.CharField(max_length=100)
+    rating = models.DecimalField(max_digits=3, decimal_places=1)
+    is_open = models.BooleanField(default=True)
+    city = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ["-rating"]
+
+    def __str__(self):
+        return f"{self.name} ({self.city})"
--- querysets/views.py
+++ querysets/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+import random
+from .models import Restaurant
+from faker import Faker
 
 # Create your views here.
+def generate_res(self):
+    fake=Faker()
+    for i in range(100):
+        cuisines=["Chinese","Italian","Thai","Lebanese","Med"]
+        r=Restaurant(
+            name = fake.last_name(),
+            city = fake.city(),
+            cuisine_type = cuisines[random.randint(0,4)],
+            rating = random.randint(1,5),
+            is_open = getOpenState()
+        )
+        r.save()
+
+def getOpenState():
+    xd = random.randint(0,1)
+    if xd==1:
+        return True
+    else:
+        return False

36cacd785e2b1737a24489552a2bf8471f50ff7b
Sankalp Boolchandani <boolchandanisankalp@gmail.com>
2025-12-15 01:19:48 +0530

    filter nultiple conditions

--- querysets/scripts/orm_script.py
+++ querysets/scripts/orm_script.py
@@ -9,10 +9,14 @@ def run():
     # print(Restaurant.objects.filter(cuisine_type='Thai'))
 
 # get()
-    print(Restaurant.objects.get(name='Page'))
+    # print(Restaurant.objects.get(name='Page'))
 
 # exists()
-    print(Restaurant.objects.filter(name='Page').exists())
-    print(Restaurant.objects.filter(name='xcvbn').exists())
+    # print(Restaurant.objects.filter(name='Page').exists())
+    # print(Restaurant.objects.filter(name='xcvbn').exists())
+
+# filter with and - done using a (,) comma
+    print(Restaurant.objects.filter(name='Page', cuisine_type='Med'))
+    print(Restaurant.objects.filter(cuisine_type='Med', name__startswith='A'))
 
     pprint(connection.queries)

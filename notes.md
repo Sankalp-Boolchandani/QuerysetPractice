f5210248c12f382e3d7383e4229a4af93789fbf9
Sankalp Boolchandani <boolchandanisankalp@gmail.com>
2025-12-16 00:48:57 +0530

    lt/lte and gt/gte lookups

--- notes.md
+++ notes.md
@@ -16,3 +16,5 @@
 # lookups
 - __startswith = checks if a string starts with a certain char or substring
 - __in = used to check if a value is in a array or no
+- __lt/lte = less than/less than equal to: for filtering out values less than specified condition
+- __gt/gte = greater than/greater than equal to: for filtering out values greater than specified condition
--- querysets/scripts/orm_script.py
+++ querysets/scripts/orm_script.py
@@ -19,13 +19,19 @@ def run():
     # print(Restaurant.objects.filter(name='Page', cuisine_type='Med'))
     # print(Restaurant.objects.filter(cuisine_type='Med', name__startswith='A'))
 
-    ch='Chinese'
-    it='Italian'
-    xd=[ch, it]
+    # ch='Chinese'
+    # it='Italian'
+    # xd=[ch, it]
     # print(Restaurant.objects.filter(cuisine_type__in=xd))
 
 # exclude()
-    print(Restaurant.objects.exclude(cuisine_type='Chinese'))
-    print(Restaurant.objects.exclude(cuisine_type__in=xd))
+    # print(Restaurant.objects.exclude(cuisine_type='Chinese'))
+    # print(Restaurant.objects.exclude(cuisine_type__in=xd))
+
+# lt/lte and gt/gte lookups
+    print(Restaurant.objects.filter(rating__gt=4))
+    print(Restaurant.objects.filter(rating__gte=4))
+    print(Restaurant.objects.filter(rating__lt=4))
+    print(Restaurant.objects.filter(rating__lte=4))
 
     # pprint(connection.queries)
